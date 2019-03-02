15e1628cf02a6dd8cb25dca265fffe20726aa83a
mykola <mykola.tokariev@gmail.com>
2019-03-02 16:05:06 +0100

    Add new panels

--- Patientverwaltung v2.0/Form1.Designer.cs
+++ Patientverwaltung v2.0/Form1.Designer.cs
@@ -39,12 +39,20 @@
             this.panel3 = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.patientsControl = new Patientverwaltung_v2._0.PatientsControl();
-            this.termineControl = new Patientverwaltung_v2._0.TermineControl();
-            this.addPatientControl = new Patientverwaltung_v2._0.AddPatientControl();
+            this.panelPatient = new System.Windows.Forms.Panel();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridViewPatient = new System.Windows.Forms.DataGridView();
+            this.panelTermine = new System.Windows.Forms.Panel();
+            this.dataGridViewTermine = new System.Windows.Forms.DataGridView();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.LeftPanel.SuspendLayout();
             this.panel3.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panelPatient.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPatient)).BeginInit();
+            this.panelTermine.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTermine)).BeginInit();
             this.SuspendLayout();
             // 
             // LeftPanel
@@ -93,7 +101,7 @@
             this.btnLogin.Name = "btnLogin";
             this.btnLogin.Size = new System.Drawing.Size(190, 57);
             this.btnLogin.TabIndex = 2;
-            this.btnLogin.Text = "Login";
+            this.btnLogin.Text = "Info";
             this.btnLogin.UseVisualStyleBackColor = true;
             // 
             // btnTermine
@@ -139,7 +147,7 @@
             this.panel3.Controls.Add(this.pictureBox1);
             this.panel3.Location = new System.Drawing.Point(275, 7);
             this.panel3.Name = "panel3";
-            this.panel3.Size = new System.Drawing.Size(106, 138);
+            this.panel3.Size = new System.Drawing.Size(106, 94);
             this.panel3.TabIndex = 2;
             // 
             // pictureBox1
@@ -162,37 +170,77 @@
             this.label1.TabIndex = 4;
             this.label1.Text = "Patientenverwaltung";
             // 
-            // patientsControl
+            // panelPatient
             // 
-            this.patientsControl.BackColor = System.Drawing.SystemColors.Control;
-            this.patientsControl.Location = new System.Drawing.Point(202, 151);
-            this.patientsControl.Name = "patientsControl";
-            this.patientsControl.Size = new System.Drawing.Size(776, 434);
-            this.patientsControl.TabIndex = 5;
+            this.panelPatient.Controls.Add(this.button3);
+            this.panelPatient.Controls.Add(this.button2);
+            this.panelPatient.Controls.Add(this.button1);
+            this.panelPatient.Controls.Add(this.dataGridViewPatient);
+            this.panelPatient.Location = new System.Drawing.Point(202, 153);
+            this.panelPatient.Name = "panelPatient";
+            this.panelPatient.Size = new System.Drawing.Size(776, 410);
+            this.panelPatient.TabIndex = 7;
             // 
-            // termineControl
+            // button1
             // 
-            this.termineControl.BackColor = System.Drawing.SystemColors.Control;
-            this.termineControl.Location = new System.Drawing.Point(202, 151);
-            this.termineControl.Name = "termineControl";
-            this.termineControl.Size = new System.Drawing.Size(776, 434);
-            this.termineControl.TabIndex = 6;
+            this.button1.Location = new System.Drawing.Point(653, 347);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(113, 51);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Bearbeiten";
+            this.button1.UseVisualStyleBackColor = true;
             // 
-            // addPatientControl
+            // dataGridViewPatient
             // 
-            this.addPatientControl.Location = new System.Drawing.Point(202, 151);
-            this.addPatientControl.Name = "addPatientControl";
-            this.addPatientControl.Size = new System.Drawing.Size(776, 434);
-            this.addPatientControl.TabIndex = 7;
+            this.dataGridViewPatient.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridViewPatient.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewPatient.Location = new System.Drawing.Point(3, 36);
+            this.dataGridViewPatient.Name = "dataGridViewPatient";
+            this.dataGridViewPatient.Size = new System.Drawing.Size(764, 298);
+            this.dataGridViewPatient.TabIndex = 0;
+            // 
+            // panelTermine
+            // 
+            this.panelTermine.Controls.Add(this.dataGridViewTermine);
+            this.panelTermine.Location = new System.Drawing.Point(230, 107);
+            this.panelTermine.Name = "panelTermine";
+            this.panelTermine.Size = new System.Drawing.Size(776, 407);
+            this.panelTermine.TabIndex = 2;
+            // 
+            // dataGridViewTermine
+            // 
+            this.dataGridViewTermine.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridViewTermine.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewTermine.Location = new System.Drawing.Point(3, 52);
+            this.dataGridViewTermine.Name = "dataGridViewTermine";
+            this.dataGridViewTermine.Size = new System.Drawing.Size(767, 291);
+            this.dataGridViewTermine.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(7, 344);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(53, 51);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "+";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(66, 344);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(57, 51);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "-";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(978, 585);
-            this.Controls.Add(this.addPatientControl);
-            this.Controls.Add(this.termineControl);
-            this.Controls.Add(this.patientsControl);
+            this.Controls.Add(this.panelTermine);
+            this.Controls.Add(this.panelPatient);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.panel3);
             this.Controls.Add(this.panel2);
@@ -200,9 +248,14 @@
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.LeftPanel.ResumeLayout(false);
             this.panel3.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panelPatient.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewPatient)).EndInit();
+            this.panelTermine.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewTermine)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -220,9 +273,13 @@
         private System.Windows.Forms.Button btnTermine;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox pictureBox1;
-        private PatientsControl patientsControl;
-        private TermineControl termineControl;
-        private AddPatientControl addPatientControl;
+        private System.Windows.Forms.Panel panelPatient;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridViewPatient;
+        private System.Windows.Forms.Panel panelTermine;
+        private System.Windows.Forms.DataGridView dataGridViewTermine;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
     }
 }
 
--- Patientverwaltung v2.0/Form1.cs
+++ Patientverwaltung v2.0/Form1.cs
@@ -7,38 +7,115 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Patientverwaltung_v2._0
 {
     public partial class Form1 : Form
     {
+        List<Panel> listPanel = new List<Panel>();
+
         public Form1()
         {
             InitializeComponent();
             SidePanel.Height = btnPatienten.Height;
             SidePanel.Top = btnPatienten.Top;
-            patientsControl.BringToFront();
         }
 
         private void btnPatienten_Click(object sender, EventArgs e)
         {
+            panelTermine.SendToBack();
+            panelPatient.BringToFront();
+
+
             SidePanel.Height = btnPatienten.Height;
             SidePanel.Top = btnPatienten.Top;
-            patientsControl.BringToFront();
+
+            var dbCon = DBConnection.Instance();
+            dbCon.DatabaseName = "db_patientenverwaltung";
+
+
+            if (dbCon.IsConnect())
+            {              
+
+                string query = string.Format("SELECT * FROM patienten");
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+
+                try
+                {
+                    MySqlDataAdapter sda = new MySqlDataAdapter();
+                    sda.SelectCommand = cmd;
+                    DataTable dbdataset = new DataTable();
+                    sda.Fill(dbdataset);
+                    BindingSource bSource = new BindingSource();
+
+                    bSource.DataSource = dbdataset;
+                    dataGridViewPatient.DataSource = bSource;
+                    sda.Update(dbdataset);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+
+                dbCon.Close();
+            }
+
         }
 
         private void btnTermine_Click(object sender, EventArgs e)
         {
+            panelTermine.BringToFront();
+
+
             SidePanel.Height = btnTermine.Height;
             SidePanel.Top = btnTermine.Top;
-            termineControl.BringToFront();
+
+            var dbCon = DBConnection.Instance();
+            dbCon.DatabaseName = "db_patientenverwaltung";
+
+
+            if (dbCon.IsConnect())
+            {
+
+                string query = string.Format("SELECT * FROM termine");
+                var cmd = new MySqlCommand(query, dbCon.Connection);
+
+                try
+                {
+                    MySqlDataAdapter sda = new MySqlDataAdapter();
+                    sda.SelectCommand = cmd;
+                    DataTable dbdataset = new DataTable();
+                    sda.Fill(dbdataset);
+                    BindingSource bSource = new BindingSource();
+
+                    bSource.DataSource = dbdataset;
+                    dataGridViewTermine.DataSource = bSource;
+                    sda.Update(dbdataset);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+
+
+                dbCon.Close();
+            }
+
         }
 
         private void btnNewPatient_Click(object sender, EventArgs e)
         {
             SidePanel.Height = btnTermine.Height;
             SidePanel.Top = btnNewPatient.Top;
-            addPatientControl.BringToFront();
+ 
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            listPanel.Add(panelPatient);
+            listPanel.Add(panelTermine);
         }
     }
 }
